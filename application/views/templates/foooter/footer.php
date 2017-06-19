8f4bf94d3ede5e8c8c303b0ff030bf98c01bb21a
MRuniversee <renatobylyk@gmail.com>
2017-06-19 15:08:42 +0300

    header and footer templates

---
+++ application/views/templates/foooter/footer.php
@@ -0,0 +1,7 @@
+  <!-- jQuery ------------------------------------------------------------------------------------------>
+  <script type="text/javascript" src="<?php echo base_url() ?>mboufos/assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <!-- Chat ------------------------------------------------------------------------------------------>
+  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/chat.js"></script>
+
+  </body>
+</html>
---
+++ application/views/templates/header/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CodeIgniter.3-Facebook.like.chat</title>
+
+    <!-- CHAT CSS ------------------------------------------------------------------->
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/chat.css">
+    
+  </head>
