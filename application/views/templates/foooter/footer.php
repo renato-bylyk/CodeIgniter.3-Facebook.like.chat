81a8e64c031eaf824a9baa229abc86f7d882a46d
MRuniversee <renatobylyk@gmail.com>
2017-06-19 15:10:02 +0300

    jquery add

--- application/views/templates/foooter/footer.php
+++ application/views/templates/foooter/footer.php
@@ -1,5 +1,5 @@
   <!-- jQuery ------------------------------------------------------------------------------------------>
-  <script type="text/javascript" src="<?php echo base_url() ?>mboufos/assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <!-- Chat ------------------------------------------------------------------------------------------>
   <script type="text/javascript" src="<?php echo base_url() ?>assets/js/chat.js"></script>
 
