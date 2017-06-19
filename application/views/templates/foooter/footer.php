2f722c1082b6ed3282bb3e34799249c1c2b23c1d
MRuniversee <renatobylyk@gmail.com>
2017-06-19 15:20:46 +0300

    new footer

--- application/views/templates/foooter/footer.php
+++
@@ -1,7 +0,0 @@
-  <!-- jQuery ------------------------------------------------------------------------------------------>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <!-- Chat ------------------------------------------------------------------------------------------>
-  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/chat.js"></script>
-
-  </body>
-</html>
